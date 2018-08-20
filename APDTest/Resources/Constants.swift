b21678c8977f9a901c67ee3959d0f0a73442d2d7
Yaroslav Skachkov <ikanom@mail.ru>
2018-08-20 16:09:20 +0300

    Constants file with width of different screens was added.

---
+++ APDTest/Resources/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  APDTest
+//
+//  Created by Yaroslav Skachkov on 20.08.2018.
+//  Copyright © 2018 Yaroslav Skachkov. All rights reserved.
+//
+
+import UIKit
+
+let currentScreenWidth = UIScreen.main.bounds.width
+let iPhone678XWidth: CGFloat = 375
+let iPhone5SEWidth: CGFloat = 320
+let iPhonePlusWidth: CGFloat = 414
